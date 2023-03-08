7bf50e5bb093aecca9b533b943324fe713903ca7
Do Tam <dolethanhtam1022@gmail.com>
2023-03-08 11:18:45 +0700

    draw line

--- app/src/main/java/com/task/DrawShape.kt
+++ app/src/main/java/com/task/DrawShape.kt
@@ -1,10 +1,7 @@
 package com.task
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
@@ -18,27 +15,35 @@ class DrawShape(context: Context, attrs: AttributeSet? = null) : View(context, a
   override fun onDraw(canvas: Canvas?) {
     super.onDraw(canvas)
     val color1 = ContextCompat.getColor(context,R.color.orange)
-    val cornerRadius = 20f
+    val cornerRadius = 25f
+    //round
+    val radii = floatArrayOf(
+      0f, 0f,
+      0f,0f,
+      cornerRadius, cornerRadius,
+      cornerRadius, cornerRadius,
+    )
     // Vẽ hình
     val path = Path()
     path.apply {
       moveTo(0f, 0f)
+      lineTo(0f + 7.5f*width/12f,0f)
+      lineTo(0f + 8*width/12f,0f + 1.5f*height/12f)
+      lineTo(0f + 8.5f*width/12f,0f)
       lineTo(0f + width, 0f)
       lineTo(0f + width, 0f + height)
       lineTo(0f, 0f + height)
+      //addRoundRect(RectF(0f + height, 0f + width, 0f + height, 0f + width),radii,Path.Direction.CW)
     }
     path.close()
-    // Tô màu
-    val fillPaint = Paint()
-    fillPaint.style = Paint.Style.FILL
-    fillPaint.color = color1
-    canvas?.drawPath(path, fillPaint)
-
-    // Vẽ đường viền
-    val strokePaint = Paint()
-    strokePaint.style = Paint.Style.STROKE
-    strokePaint.strokeWidth = 1f
-    strokePaint.color = color1
-    canvas?.drawPath(path, strokePaint)
+    val pain = Paint()
+    pain.apply {
+      style = Paint.Style.FILL_AND_STROKE
+      color = color1
+      /*strokeJoin = Paint.Join.ROUND
+      strokeCap = Paint.Cap.ROUND
+      pathEffect = CornerPathEffect(cornerRadius)*/
+    }
+    canvas?.drawPath(path,pain)
   }
 }
